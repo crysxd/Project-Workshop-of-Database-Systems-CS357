2a3009563dfc4bcb9cd4fc3d948817ef8e1d13e3
crysxd <c.wuerthner@me.com>
2015-04-20 23:30:39 +0800

    Implemented index page

---
+++ Webpage/js/index.js
@@ -0,0 +1,17 @@
+if(/mobile|android|iOS|iPhone|iPad/i.test(navigator.userAgent)) {
+  $('#splash-screen').height($(window).height() - $('#top-menu').height() - $('#splash-screen').css('margin-bottom'));
+}
+
+$('.menu-toggle').click(function() {
+  if($('#menu:visible').length > 0) {
+    $('#menu').fadeOut();
+    $('.menu-toggle').removeClass('menu-toggle-open');
+    $('#login-button').css('opacity', '');
+  } else {
+    $('#menu').fadeIn().css('display','table');
+    $('.menu-toggle').addClass('menu-toggle-open');
+    $('#menu').addClass('menu-open');
+    loginWasVisible = $('#login-button').css('opacity');
+    $('#login-button').css('opacity', '1');
+  }    
+});
