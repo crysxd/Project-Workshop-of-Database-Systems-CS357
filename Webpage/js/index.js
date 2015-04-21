85682e4d1a6da9610cccd5aa9460cc5b8ca44b7f
crysxd <c.wuerthner@me.com>
2015-04-21 14:44:03 +0800

    Added JS code for login form

--- Webpage/js/index.js
+++ Webpage/js/index.js
@@ -7,15 +7,54 @@ $('.menu-toggle').click(function() {
     $('#menu').fadeOut();
     $('.menu-toggle').removeClass('menu-toggle-open');
     $('#login-button').css('opacity', '');
+    startScrolling();
+
   } else {
     $('#menu').fadeIn().css('display','table');
     $('.menu-toggle').addClass('menu-toggle-open');
     $('#menu').addClass('menu-open');
-    loginWasVisible = $('#login-button').css('opacity');
     $('#login-button').css('opacity', '1');
+    stopScrolling();
   }    
 });
 
 $('#splash-screen .search-box').click(function() {
   $(this).find('p').html('Searching...');
 });
+
+$('#login-button').click(function() {
+  $('#login-overlay').fadeIn().css('display','table');
+  stopScrolling();
+});
+
+$('#register-button').click(function() {
+  $('#register-button').animate({opacity: 0});
+  $('#task-register').slideDown().fadeIn();
+  $('#task-login').slideUp().fadeOut();
+  updateScrollerHeight();
+});
+
+$('#login-cancel-button').click(function() {
+  $('#login-overlay').fadeOut(function() {
+    $('#register-button').css('opacity', 1);
+    $('#task-register').hide();
+    $('#task-login').show();
+  });
+  startScrolling();
+});
+
+$(window).resize(function() {
+  updateScrollerHeight();
+});
+
+function updateScrollerHeight() {
+    $('.scroll').css('max-height', $(window).height() + 'px');
+}
+
+function stopScrolling() {
+  $('body').css('overflow', 'hidden');
+}
+
+function startScrolling() {
+  $('body').css('overflow', 'scroll');
+}
