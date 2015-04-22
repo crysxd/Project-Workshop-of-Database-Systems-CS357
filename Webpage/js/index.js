5fc4ec6f6d9e8d415052a095681406094ae26115
crysxd <c.wuerthner@me.com>
2015-04-22 15:24:39 +0800

    Cleaned branch

--- Webpage/js/index.js
+++
@@ -1,9 +0,0 @@
-/* Click handler for search box */
-$('#splash-screen .search-box').click(function() {
-  leaveTo('restaurants.php');
-});
-
-/* When the doc is ready fade out the loading screen after 1s */
-$(document).ready(function() {
-  window.setTimeout(hideLoadingOverlay, 250);
-});
--- Webpage/js/restaurants.js
+++
@@ -1,4 +0,0 @@
-/* When the doc is ready fade out the loading screen after 1s */
-$(document).ready(function() {
-  window.setTimeout(hideLoadingOverlay, 1000);
-});
