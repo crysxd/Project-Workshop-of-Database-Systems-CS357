4b0f3d03ea4b41409d29844fd80dc7ef73cb47df
crysxd <c.wuerthner@me.com>
2015-04-24 13:37:05 +0800

    Added empty meals page

---
+++ Webpage/js/meals.js
@@ -0,0 +1,4 @@
+/* When the doc is ready fade out the loading screen after 1s */
+$(document).ready(function() {
+  window.setTimeout(hideLoadingOverlay, 250);
+});
---
+++ Webpage/meals.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+
+  <title>my-burger.com | Meals</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Custom styles for this template -->
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/meals.css" rel="stylesheet">
+</head>
+<body>
+  
+  <!-- Menus and overlays -->
+  <?php  include('shared/header.html'); ?>
+  
+  <div id="main-content">
+    
+    <!-- How to order list -->
+    <section id="meals">
+      
+    </section>
+
+    <!-- Footer -->
+    <?php include('shared/footer.html'); ?>
+    
+  </div>
+  
+  <!-- Load Scripts at the end to optimize site loading time -->
+  <script src="js/jquery-2.1.0.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/meals.js"></script>
+</body>
+</html>
