63636f470b0a09c3771f7a81fa07dad688395efe
crysxd <c.wuerthner@me.com>
2015-04-25 09:47:26 +0800

    Implemented side drawer for orders

--- Webpage/js/meals.js
+++ Webpage/js/meals.js
@@ -2,3 +2,13 @@
 $(document).ready(function() {
   window.setTimeout(hideLoadingOverlay, 250);
 });
+
+$('#drawer-toggle, #left-drawer').click(function(e) {
+  $('#left-drawer').toggleClass('drawer-open');
+  $('#main-contant-overlay').toggleClass('visible');
+  e.stopPropagation();
+});
+
+$('#main-contant-overlay').click(function() {
+  $('#drawer-toggle').click();
+});
