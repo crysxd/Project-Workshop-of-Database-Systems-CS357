c1fa06b5a1dd8d202b4ea377bf4a174db4f8197c
crysxd <c.wuerthner@me.com>
2015-06-10 19:57:33 +0800

    Fixed missing class for icon element

--- Webpage/meals.php
+++ Webpage/meals.php
@@ -48,7 +48,7 @@
           
           <!-- Info box -->
           <div id="restaurant-info">
-            <img src="imgs/placeholder.png" class="pull-left" alt>
+            <img src="imgs/placeholder.png" class="pull-left restaurant-icon" alt>
             <h4 class="restaurant-name">undefined</h4>
             <p><span class="restaurant-rating-stars">☆☆☆☆☆</span>&nbsp;(<span class="restaurant-rating-count">undefined</span>)</p>
             <p>Minimum order value:&nbsp;<span class="restaurant-min-order-value">undefined</span>元</p>
