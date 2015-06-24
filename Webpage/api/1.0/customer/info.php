5ebaaf6c0fc5ab71e3c92946d7a4053559a20277
crysxd <c.wuerthner@me.com>
2015-06-24 14:24:57 +0800

    Fixed query for ongoing and old orders

--- Webpage/api/1.0/customer/info.php
+++ Webpage/api/1.0/customer/info.php
@@ -153,56 +153,30 @@
     ";
     
     $stmt_select_ongoing_deliveries = "
-        SELECT * 
-        FROM (
-          SELECT cdds.delivery_id_pk id, r.name restaurant, dst.name state, cdds.date_pk state_since
-          FROM (
-            SELECT d.delivery_id_pk, d.Restaurant_restaurant_id, ds.date_pk, ds.Delivery_State_Type_delivery_status_type
-            TYPE 
-            FROM (
-              SELECT customer_id_pk
-              FROM Customer
-              WHERE nick =  ?
-            )c
-            INNER JOIN Delivery d ON c.customer_id_pk = d.Customer_customer_id
-            INNER JOIN Delivery_State ds ON d.delivery_id_pk = ds.Delivery_delivery_id_pk
-            WHERE d.delivery_id_pk != ( 
-              SELECT d.delivery_id_pk
-              FROM (
-                SELECT customer_id_pk
-                FROM Customer
-                WHERE nick =  ?
-              )c
-              INNER JOIN Delivery d ON c.customer_id_pk = d.Customer_customer_id
-              INNER JOIN Delivery_State ds ON d.delivery_id_pk = ds.Delivery_delivery_id_pk
-              WHERE ds.Delivery_State_Type_delivery_status_type = 4 ) 
-          )cdds
-          INNER JOIN Delivery_State_Type dst ON cdds.type = dst.delivery_status_type_id_pk
-          INNER JOIN Restaurant r ON cdds.Restaurant_restaurant_id = r.restaurant_id_pk
-          ORDER BY state_since DESC
-        )cddsr
-        GROUP BY cddsr.id
+      SELECT d.delivery_id_pk AS id, r.name AS restaurant, m.name AS state, s.date_pk AS state_since
+      FROM delivery AS d
+      INNER JOIN (
+          SELECT max(Delivery_State_Type_delivery_status_type) AS state, delivery_delivery_id_pk, max(date_pk) AS date_pk
+          FROM delivery_state
+          GROUP BY delivery_delivery_id_pk 
+      ) AS s on d.delivery_id_pk = s.delivery_delivery_id_pk
+      INNER JOIN customer AS c ON d.customer_customer_id = c.customer_id_pk
+      INNER JOIN delivery_state_type AS m ON s.state = m.delivery_status_type_id_pk
+      INNER JOIN restaurant AS r ON d.restaurant_restaurant_id = r.restaurant_id_pk
+      WHERE c.nick = ? AND s.state != 4
     ";
     $stmt_select_old_deliveries = "
-        SELECT * 
-        FROM (
-          SELECT cdds.delivery_id_pk id, r.name restaurant, dst.name state, cdds.date_pk state_since
-          FROM (
-            SELECT d.delivery_id_pk, d.Restaurant_restaurant_id, ds.date_pk, ds.Delivery_State_Type_delivery_status_type type
-            FROM (
-              SELECT customer_id_pk
-              FROM Customer
-              WHERE nick =  ?
-            )c
-            INNER JOIN Delivery d ON c.customer_id_pk = d.Customer_customer_id
-            INNER JOIN Delivery_State ds ON d.delivery_id_pk = ds.Delivery_delivery_id_pk
-            WHERE ds.Delivery_State_Type_delivery_status_type = 4
-          )cdds
-          INNER JOIN Delivery_State_Type dst ON cdds.type = dst.delivery_status_type_id_pk
-          INNER JOIN Restaurant r ON cdds.Restaurant_restaurant_id = r.restaurant_id_pk
-          ORDER BY state_since DESC
-        )cddsr
-        GROUP BY cddsr.id
+      SELECT d.delivery_id_pk AS id, r.name AS restaurant, m.name AS state, s.date_pk AS state_since
+      FROM delivery AS d
+      INNER JOIN (
+          SELECT max(Delivery_State_Type_delivery_status_type) AS state, delivery_delivery_id_pk, max(date_pk) AS date_pk
+          FROM delivery_state
+          GROUP BY delivery_delivery_id_pk 
+      ) AS s on d.delivery_id_pk = s.delivery_delivery_id_pk
+      INNER JOIN customer AS c ON d.customer_customer_id = c.customer_id_pk
+      INNER JOIN delivery_state_type AS m ON s.state = m.delivery_status_type_id_pk
+      INNER JOIN restaurant AS r ON d.restaurant_restaurant_id = r.restaurant_id_pk
+      WHERE c.nick = ? AND s.state = 4
     ";
     
     $answer['success'] = true;
@@ -225,7 +199,7 @@
     
     $answer['ongoing_deliveries'] = array();
     //
-    if(!($select_ongoing_deliveries_result = push_stmt($stmt_select_ongoing_deliveries, "ss", array(&$args['user'], &$args['user']))))
+    if(!($select_ongoing_deliveries_result = push_stmt($stmt_select_ongoing_deliveries, "s", array(&$args['user']))))
       db_error($answer);
     
     while($select_ongoing_deliveries_result && ($fetched_ongoing_deliveries_dishes_row = $select_ongoing_deliveries_result->fetch_assoc()))
